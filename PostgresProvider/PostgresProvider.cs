6c4c14f99ca765b25892dd4425b3e16a65cf7177
K Cartlidge <code@kcartlidge.com>
2020-01-05 18:15:18 +0000

    Add PostgresProvider

---
+++ PostgresProvider/PostgresProvider.cs
@@ -0,0 +1,83 @@
+﻿using Migratable.Interfaces;
+using Npgsql;
+
+namespace Migratable.Providers
+{
+    public class PostgresProvider : IProvider
+    {
+        private readonly string connectionString;
+
+        public PostgresProvider(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public string Describe()
+        {
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                return $"Postgres against {conn.DataSource} {conn.Database}";
+            }
+        }
+
+        public void Execute(string instructions)
+        {
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                var transaction = conn.BeginTransaction();
+                var cmd = new NpgsqlCommand(instructions, conn);
+                cmd.Transaction = transaction;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    transaction.Commit();
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+                conn.Close();
+            }
+        }
+
+        public int GetVersion()
+        {
+            var v = ExecuteScalar(
+                "CREATE TABLE IF NOT EXISTS migratable_version" +
+                "(" +
+                "    id integer NOT NULL GENERATED ALWAYS AS IDENTITY(INCREMENT 1 START 1), " +
+                "    version_number integer NOT NULL, " +
+                "    actioned timestamp with time zone NOT NULL DEFAULT NOW(), " +
+                "    PRIMARY KEY(id) " +
+                ");"
+                );
+            v = ExecuteScalar("select version_number from migratable_version order by id desc limit 1");
+            if (v == null)
+            {
+                SetVersion(0);
+                return 0;
+            }
+            return (int)v;
+        }
+
+        public void SetVersion(int versionNumber)
+        {
+            var sql = "insert into migratable_version (version_number) values ({0})";
+            Execute(string.Format(sql, versionNumber));
+        }
+
+        private object ExecuteScalar(string instructions)
+        {
+            using (var conn = new NpgsqlConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = new NpgsqlCommand(instructions, conn);
+                var result = cmd.ExecuteScalar();
+                conn.Close();
+                return result;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+# Migratable.PostgresProvider
+
+Database provider for https://www.nuget.org/packages/Migratable to add support for *Postgres*.
+
+## Installation
+
+Both this provider and the main Migratable are on Nuget.
+
+## Usage
+
+See https://www.nuget.org/packages/Migratable for details.
+
+``` cs
+var provider = new Migratable.Providers.PostgresProvider("connection-string");
+var migrator = new Migratable.Migrator(provider);
+migrator.LoadMigrations("./migrations");
+migrator.SetVersion(5);
+```
+
+---
+
+## MIT Licence
+
+Copyright (c) 2020 K Cartlidge.
+See ```LICENCE``` file for details.
